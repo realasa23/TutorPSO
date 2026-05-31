86543945d2f84bcf4de25731fca039061c55b859
tahiquda <halizameliani@gmail.com>
2026-05-31 20:36:17 +0700

    Fix model primary key dan mengembalikan semua controller ke logic database asli

--- app/Http/Controllers/matakuliahController.php
+++ app/Http/Controllers/matakuliahController.php
@@ -1,21 +1,22 @@
 <?php
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
-// Nailah Adlina - 5026231068
+//Nailah Adlina - 5026231068
 
 class matakuliahController extends Controller
 {
     public function materi($idkategori)
     {
-        // --- BYPASS SEMENTARA ---
-        $kategori = (object) ['idkategori' => $idkategori, 'namakategori' => 'Kategori Dummy'];
-        
-        // Kasih dummy matkul biar UI nggak kosong
-        $matakuliah = collect([
-            (object)['idmatkul' => 1, 'namamatkul' => 'Matkul Dummy 1', 'jumlah_sesi' => 3],
-            (object)['idmatkul' => 2, 'namamatkul' => 'Matkul Dummy 2', 'jumlah_sesi' => 5]
-        ]);
+        // Kodingan Asli Nembak Database
+        $kategori = DB::table('kategori')
+            ->where('idkategori', $idkategori)
+            ->first();
+
+        $matakuliah = DB::table('matakuliah')
+            ->where('idkategori', $idkategori)
+            ->get();
 
         return view('List-Materi', compact('kategori', 'matakuliah'));
     }
--- app/Http/Controllers/tutorController.php
+++ app/Http/Controllers/tutorController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 // Harya Raditya Handoyo - 5026231176
 // Nailah Adlina - 5026231068
@@ -9,48 +10,10 @@ class tutorController extends Controller
 {
     public function recTutor()
     {
-        // --- BYPASS SEMENTARA: DUMMY DATA TUTOR ---
-        $tutor = collect([
-            (object)[
-                'idtutor' => 1,
-                'nama' => 'Sasha',
-                'pekerjaan' => 'PWEB Developer',
-                'fototutor' => 'https://ui-avatars.com/api/?name=Sasha&background=random',
-                'ratingtutor' => 4.9,
-                'total_review' => 88
-            ],
-            (object)[
-                'idtutor' => 2,
-                'nama' => 'Haryadi',
-                'pekerjaan' => 'UX Design',
-                'fototutor' => 'https://ui-avatars.com/api/?name=Haryadi&background=random',
-                'ratingtutor' => 4.9,
-                'total_review' => 75
-            ],
-            (object)[
-                'idtutor' => 3,
-                'nama' => 'Khalila',
-                'pekerjaan' => 'Data Analyst',
-                'fototutor' => 'https://ui-avatars.com/api/?name=Khalila&background=random',
-                'ratingtutor' => 4.8,
-                'total_review' => 60
-            ]
-        ]);
-
+        // Kodingan Asli Nembak Database
+        $tutor = DB::table('tutor')->get();
         return view('List-Tutor', compact('tutor'));
     }
 
-    public function profile($id)
-    {
-        $tutor = (object) ['idtutor' => $id, 'nama' => 'Tutor Dummy', 'pekerjaan' => 'Tutor Profesional', 'deskripsi' => 'Deskripsi Tutor Dummy', 'fototutor' => null, 'ratingtutor' => 5, 'total_review' => 0];
-        $reviews = collect([]);
-        return view('Profile-Tutor', compact('tutor', 'reviews'));
-    }
-
-    public function listSesi($idtutor)
-    {
-        $tutor = (object) ['idtutor' => $idtutor, 'nama' => 'Tutor Dummy'];
-        $sesi = collect([]);
-        return view('Daftar-Sesi-Tutor', compact('tutor', 'sesi'));
-    }
+    // ... (Fungsi profile dan listSesi biarin pakai kodingan asli lu sebelumnya)
 }
