1daa551508b5382dbb827d1bab5a7f68b01b24fb
realasa23 <realasa23@gmail.com>
2026-06-08 23:00:13 +0700

    migrate durasi

---
+++ database/migrations/2026_06_08_225113_add_durasi_to_pesanan_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up() {
+    Schema::table('pesanan', function (Blueprint $table) {
+        $table->integer('durasi')->default(1);
+    });
+}
+public function down() {
+    Schema::table('pesanan', function (Blueprint $table) {
+        $table->dropColumn('durasi');
+    });
+}
+};
